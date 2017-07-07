1057331fa123900aecd18b2534ea9f68760140e0
mat-h <blackcloister@gmail.com>
2017-07-08 02:17:07 +0900

    35.1

--- src/main/scala/org/stairbook/scells/Spreadsheet.scala
+++ src/main/scala/org/stairbook/scells/Spreadsheet.scala
@@ -4,6 +4,15 @@ import swing._
 class Spreadsheet (val width: Int, val height: Int) extends ScrollPane {
   val table = new Table(width, height) {
     showGrid = true  
+    rowHeight = 25
+    autoResizeMode = Table.AutoResizeMode.Off
+    gridColor = new java.awt.Color(150,150,150)
   }
   viewportView = table
+  
+  val rowNumbers = new ListView ((0 until height).map(_.toString)) {
+    fixedCellWidth = 30
+    fixedCellHeight = table.rowHeight
+  }
+  rowHeaderView = rowNumbers
 }
